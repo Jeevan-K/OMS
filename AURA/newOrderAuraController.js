deeaafaf125c25320f1eedefd4979a68d1f4c4c4
Jeevan-K <39034038+Jeevan-K@users.noreply.github.com>
2020-04-22 09:28:15 +0530

    Update newOrderAuraController.js

--- AURA/newOrderAuraController.js
+++ AURA/newOrderAuraController.js
@@ -4,20 +4,29 @@
         //var flow = component.find("flowData");
         // In that component, start your flow. Reference the flow's API Name.
         //flow.startFlow("Create_Contact");
-        
+        //component.get("v.falseValue")
+        var gotID = component.get("v.recordId");
+        console.log("Record ID of edit is");
+        console.log(gotID);
+        if(gotID)
+            component.set("v.cardTtl","Edit Record");
         
     },
     
     handleSuccess: function(component, event, helper) {
         var newOrder = event.getParams().response;
         var navId = newOrder.id;
+        var tstMessage = "The Order has been created successfully.";
         console.log(navId);
+            if(component.get("v.recordId")){
+                tstMessage = "Changes saved";
+            }
         var toastEvent = $A.get("e.force:showToast");
             toastEvent.setParams({
                 "type":"success",
                 "title": "Saved",
                 "mode":"pester",
-                "message": "The Order has been created successfully."
+                "message": tstMessage
             });
             toastEvent.fire();
             
@@ -27,5 +36,14 @@
             });
             navEvt.fire();
    
+    },
+    
+    handleCancel : function(comonent,event,helper){
+        alert("inside cancel");
+        var homeEvent = $A.get("e.force:navigateToObjectHome");
+    homeEvent.setParams({
+        "scope": "Order"
+    });
+    homeEvent.fire();
     }
 })
